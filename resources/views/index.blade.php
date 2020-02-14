554821b74544823efb9b227ca47c5d5198ec5ebd
cristi <ccazacu81@gmail.com>
2020-02-14 09:20:20 +0200

    e

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,22 +38,24 @@
     </div>
   </div>
 
+  
+
   <div class="container">
     <!-- Example row of columns -->
     <div class="row">
-
-           @foreach($articles as $article)
+@foreach($articles as $article)
 
       <div class="col-md-4">
         <h2>{{$article->title}}</h2>
         <p>{{$article->description}}</p>
+        
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
-    </div>
-    <hr>
+      </div>@endforeach
+      <hr>
+    </div>  
   </div> <!-- /container -->
 
-  @endforeach
+
 
 </main>
 <footer class="container">
